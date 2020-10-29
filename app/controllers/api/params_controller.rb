d4e70f8e3efd827b756a7c7c26890d0914af62a2
Pedro <orsomethingelse@gmail.com>
2020-10-29 14:29:56 -0500

    testing first letter if

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,7 +1,11 @@
 class Api::ParamsController < ApplicationController
   def name_action
-    @name = params["your name"]
-    @output = "Your name is #{@name}"
+    @name = params["your_name"]
+    @output = "Your name is #{@name.upcase}"
+    @name_split = @name.split(//)
+    if @name_split[0] == "a"
+      return "Hey, your name starts with the first letter of the alphabet!"
+    end
     render "name.json.jb"
   end
 end
